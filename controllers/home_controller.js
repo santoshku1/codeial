1e9a56c74ad186ad235adc0916832e92841df8ba
Santosh Kumar <santoshhk1998@gmail.com>
2021-04-19 14:38:06 +0530

    installing cookie-parser and creating cookies

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,8 @@
 module.exports.home = function (req, res) {
+  console.log(req.cookies);
+  res.cookie("user_id", 45);
   //   return res.end("<h1> express is running for codeial!</h1>");
   return res.render("home", {
-    tittle: "Home",
+    title: "Home",
   });
 };
